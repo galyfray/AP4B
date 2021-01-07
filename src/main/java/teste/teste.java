7fdd096dd60ee131bb062b2260545b87ef5bbd1d
PGoubet <paul.goubet@utbm.fr>
2021-01-07 17:16:31 +0100

    teste main

---
+++ src/main/java/teste/teste.java
@@ -0,0 +1,11 @@
+package teste;
+
+public class teste
+{
+
+
+    public static void main( String[] args )
+    {
+        System.out.println("Bonjour à tous et à toute c'est parti mon quiqi");
+    }
+}
