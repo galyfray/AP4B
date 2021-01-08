e0d31fe0a64979fb14caefb06fe9d3e9e925d529
Galyfray <galyfray@gmail.com>
2021-01-08 16:15:23 +0100

    added Credit

---
+++ src/main/java/tickets/uv/Credit.java
@@ -0,0 +1,14 @@
+package tickets.uv;
+
+public enum Credit
+{
+    CS("CS"), TM("TM"), EC("EC"), QC("QC"), OM("OM");
+
+    private final String name;
+
+    Credit( String name )
+    {
+        this.name = name;
+    }
+
+}
