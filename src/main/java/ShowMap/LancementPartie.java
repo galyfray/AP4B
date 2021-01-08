b459f38dddd8929032b6700163b44dc43236f0d5
PGoubet <paul.goubet@utbm.fr>
2021-01-08 13:07:22 +0100

    lancement

---
+++ src/main/java/ShowMap/LancementPartie.java
@@ -0,0 +1,49 @@
+package ShowMap;
+
+import java.awt.*;
+import javax.swing.*;
+
+
+public class LancementPartie extends JPanel implements ActionListener
+{
+
+
+    public static void main( String[] args )
+    {
+
+        JFrame presentation = new JFrame("Lancement");
+
+        JLabel text = new JLabel("Bienvenue dans le menu de lancement des Aventuriers du Rail",JLabel.CENTER);
+        presentation.add(text);
+
+        JPanel panel1 = new JPanel();
+        JButton btn1 = new JButton("Nouvelle partie");
+        JButton btn2 = new JButton("Reprendre une partie en cours");
+        panel1.add(btn1);
+        panel1.add(btn2);
+
+        presentation.setLayout(new GridLayout(2, 1));
+        presentation.add(panel1);
+        presentation.add(text);
+
+        presentation.pack();
+        presentation.setSize(1280,720);
+        presentation.setVisible(true);
+        presentation.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        btn1.addActionListener(new ActionListener(this));
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                carte.SetVisible(true);
+                this.SetVisible(false);
+            }
+
+        }
+
+    }
+
+}
+
