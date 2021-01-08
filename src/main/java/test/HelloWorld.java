2cf275e621a70c7ab36bb4e32963470afa6a20f2
Galyfray <galyfray@gmail.com>
2021-01-08 14:54:22 +0100

    test Destination system working

--- src/main/java/test/HelloWorld.java
+++ src/main/java/test/HelloWorld.java
@@ -14,15 +14,14 @@ public class HelloWorld
         UV LP24 = new UV("LP24"), LO21 = new UV("LO21"), AP4B = new UV("AP4B"), AP4A = new UV("AP4A");
         Road ll = new Road(LO21, LP24);
         Road la = new Road(LO21, AP4A);
+        Road lab = new Road(LO21, AP4B);
         Road aa = new Road(AP4A, AP4B);
+        Road al = new Road(AP4A, LP24);
 
-        Destination linearShort = new Destination(LO21, LP24);
-        Destination linearMedium = new Destination(LP24, AP4A);
         Destination linearLong = new Destination(LP24, AP4B);
 
-        System.out.println(linearShort.isComplete(List.of(aa, la, ll)));
-        System.out.println(linearMedium.isComplete(List.of(aa, la, ll)));
-        System.out.println(linearLong.isComplete(List.of(aa, la, ll)));
+        System.out.println(linearLong.isComplete(List.of(aa, ll)));
+
     }
 
 }
