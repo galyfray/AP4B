0d88693c9318ad88181d2b516013ba1cab65ba3b
Pierre <pierre.clerc@icloud.com>
2021-01-08 22:11:34 +0100

    maj des boutons

--- src/main/java/tickets/panels/Boutons.java
+++ src/main/java/tickets/panels/Boutons.java
@@ -1,5 +1,8 @@
 package tickets.panels;
 
+import tickets.Turn;
+import tickets.uv.Credit;
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -16,7 +19,40 @@ public class Boutons extends JMenuBar
         actions.add(passerSonTourBoutton);
         passerSonTourBoutton.addActionListener(e -> System.out.println("Bonjour!"));
 
-        JMenuItem prendreUneRouteBoutton = new JMenuItem("Prendre une route");
-        actions.add(prendreUneRouteBoutton);
+        JMenu prendreUneRouteMenu = new JMenu("Prendre une route");
+        //Faire un loop pour lister toutes les routes dispo en fonction du joueur
+        JMenuItem route1 = new JMenuItem("Route 1");
+        route1.addActionListener(e -> prendreUneRoute());
+        prendreUneRouteMenu.add(route1);
+
+
+
+        actions.add(prendreUneRouteMenu);
+        //actions.add(prendreUneRouteBoutton);
+
+        JMenu joueurEnCours = new JMenu("Infos joueur");
+        this.add(joueurEnCours);
+
+        JMenuItem affichageJoueur = new JMenuItem("Nom du joueur");
+        joueurEnCours.add(affichageJoueur);
+
+        JMenuItem creditsBouton = new JMenuItem("Crédits possédés : Afficher le nombre de crédits");
+        joueurEnCours.add(creditsBouton);
+
+        //Turn tour;
+        //tour.getCurrentPlayer();
+
+    }
+
+    public void prendreUneRoute()
+    {
+
+        // Mettre à joueur actuel en fonction de la route qu'il prend.
+    }
+
+    public void passerSonTour()
+    {
+        // Mettre tous les boutons à jour:
+        // - Infos joueur (Nom du joueur, Crédits du joueur)
     }
 }
